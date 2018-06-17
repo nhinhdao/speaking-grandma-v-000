6bd5ab5269aefe596da045730f6b36bc85c6c73d
Nhinh Dao <nhinhdao.vn@gmail.com>
2018-06-17 17:09:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -16,5 +16,11 @@ def speak_to_grandma(phrase)
             "I LOVE YOU TOO PUMPKIN!"
       elsif phrase == "Hi Nana, how are you?"
             "HUH?! SPEAK UP, SONNY!"
+      elsif phrase == "Hi!"
+            "HUH?! SPEAK UP, SONNY!"
+      elsif phrase == "WHAT DID YOU EAT TODAY?"
+            "NO, NOT SINCE 1938!"
+      elsif phrase == "WHAT?"
+            "NO, NOT SINCE 1938!"
       end
 end
